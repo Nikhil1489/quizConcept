6556dd05b9fe46b2d24211a9ce0ddb8726ef15a9
Nikhil1489 <nikhil@ishir.com>
2020-06-22 22:41:57 +0530

    adds validations

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -17,6 +17,7 @@ export class RegisterComponent implements OnInit {
 
   hide = true;
   regForm: FormGroup;
+  selectedGender: string = 'male';
 
 
   locations: Dropdown[] = [
@@ -25,22 +26,30 @@ export class RegisterComponent implements OnInit {
     { value: 'india', viewValue: 'India' }
   ];
 
+  selectedLocation = this.locations[0].value;
+
   quiztypes: Dropdown[] = [
     { value: 'engineering', viewValue: 'Engineering' },
     { value: 'management', viewValue: 'Management' },
   ];
 
-  name: any;
-  email:any;
-  password:any;
+  selectedQuizType = this.quiztypes[0].value;
 
+  name: any;
+  email: any;
+  password: any;
+  sucessmessage: string = null;
+  errormessage: string = null;
 
   constructor(private fb: FormBuilder, private registerService: registerService) {
 
     this.regForm = this.fb.group({
       'name': [null, Validators.required],
-      'email': [null, Validators.required],
-      'password': [null, Validators.required]
+      'email': [null, [Validators.required, Validators.email]],
+      'password': [null, Validators.required],
+      'location': [null],
+      'gender': [null],
+      'quiztype': [null]
     })
 
   }
@@ -61,15 +70,13 @@ export class RegisterComponent implements OnInit {
 
     this.markFormGroupTouched(this.regForm);
     if (this.regForm.valid) {
-      // this.name = this.regForm.get('name').value,
-      // this.email = this.regForm.get('email').value;
-      // this.password = this.regForm.get('password').value;
-      // console.log(this.name, this.email, this.password);
-
-      this.registerService.register(this.regForm.getRawValue()).subscribe((res:any)=>{
-        console.log(res);
-      })
-
+      this.registerService.register(this.regForm.getRawValue()).subscribe((res: any) => {
+        if (res.messsage) {
+          this.sucessmessage = res.message;
+        }
+      }, (error) => {
+        this.errormessage = error.message;
+      });
     }
 
   }
